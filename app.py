e5bbad8f7cfdb577f2ade94d930755734b1f39b7
Adam Ovesen <66880800+AdamRaboch@users.noreply.github.com>
2024-10-21 21:12:15 +0300

    Update app.py with env variables

--- app.py
+++ app.py
@@ -1,36 +1,36 @@
 from flask import Flask, render_template, request, redirect
 import os
 
-
-
 # Retrieve the DATABASE_TYPE environment variable, defaulting to 'MYSQL' if not set
 db_to_use = os.getenv("DATABASE_TYPE", "MYSQL")
 
 if db_to_use == "MYSQL":
     from data_sql import (get_contacts, findByNumber,
                           check_contact_exist, search_contacts,
-                          create_contact,
-                          delete_contact, update_contact_in_db)
+                          create_contact, delete_contact, update_contact_in_db)
+
+    # Set up MySQL connection using environment variables
+    db = mysql.connector.connect(
+        host=os.getenv("MYSQL_HOST"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        database=os.getenv("MYSQL_DATABASE"),
+        port=os.getenv("MYSQL_PORT", 3306)
+    )
 elif db_to_use == "MONGO":
     from data_mongo import (get_contacts, findByNumber,
                             check_contact_exist, search_contacts,
-                            create_contact,
-                            delete_contact, update_contact_in_db)
+                            create_contact, delete_contact, update_contact_in_db)
 
 app = Flask(__name__)
-
-
-
 
 @app.route('/')
 def hello():
     return redirect('/viewContacts')
 
-
 @app.route('/addContact', methods=['GET', 'POST'])
 def addContact():
     return render_template('addContactForm.html')
-
 
 # route to view the contact list
 @app.route('/viewContacts')
@@ -38,14 +38,12 @@ def viewContacts():
     print(get_contacts())
     return render_template('index.html', contacts=get_contacts())
 
-
-
 @app.route('/createContact', methods=['POST'])
 def createContact():
     # adding additional contact to the database (contacts_list)
     fullname = request.form['fullname']
     email = request.form['email']
-    phone = request.form['phone']   
+    phone = request.form['phone']
     gender = request.form['gender']
     photo = request.files['photo']
     if not check_contact_exist(fullname, email):
@@ -65,7 +63,6 @@ def deleteContact(number):
     delete_contact(number)
     return redirect('/viewContacts')
 
-
 @app.route('/editContact/<number>')
 def editContact(number):
     contact = findByNumber(number)
@@ -80,15 +77,12 @@ def saveUpdatedContact(number):
     update_contact_in_db(number, name, phone, email, gender)
     return redirect('/viewContacts')
 
-
 # search route to filter the contact list according to the search criteria:
 @app.route('/search', methods=['POST'])
 def search():
     search_name = request.form['search_name']
     search_results = search_contacts(search_name)
     return render_template('index.html', contacts=search_results)
-
-
 
 if __name__ == '__main__':
     app.run(debug=True ,port=5052,  host='0.0.0.0')
